8fbdf09f75381b1373ebec3885eaf9951e461758
salmanfarzii <salman@hexwhale.com>
2021-10-27 22:07:32 +0530

    generate JWT token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import config from '../config/keys';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: config.JWTSECRET,
+      signOptions: { expiresIn: config.JWTEXPIRESIN },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { AuthDto } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(authDto: AuthDto) {
     const user = await this.usersService.findOne({ email: authDto.email });
@@ -15,6 +19,17 @@ export class AuthService {
     if (!(await bcrypt.compare(authDto.password, user.password)))
       throw new UnauthorizedException('Invalid Credentials!');
 
-    return user;
+    const access_token = this.jwtService.sign({
+      name: user.name,
+      sub: user.id,
+      role: user.role,
+    });
+
+    const result = {
+      message: 'Login Successfull!',
+      access_token,
+    };
+
+    return result;
   }
 }
